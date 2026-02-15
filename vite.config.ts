f11743e17ae6f43aaa71419f345c72a044469f28
Fakerieh <90840493+fakerieh@users.noreply.github.com>
2026-02-15 10:34:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/Custom-ROMs-App/",  // <--- TAMBAHKAN BARIS INI (PENTING!)
+})
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
